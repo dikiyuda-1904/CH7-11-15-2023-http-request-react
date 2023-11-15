6ea95cfba76e5a15b019da57054d712c7f2c5665
ki-yud19 <dickyudhz@gmail.com>
2023-11-15 22:35:06 +0700

    "first commit"

--- src/components/AvailablePlaces.jsx
+++ src/components/AvailablePlaces.jsx
@@ -1,10 +1,51 @@
-import Places from './Places.jsx';
+import { useEffect, useState } from "react";
+
+import Places from "./Places.jsx";
+import Error from "./Error.jsx";
+import { sortPlacesByDistance } from "../loc.js";
+import { fetchPlaces } from "../http.js";
 
 export default function AvailablePlaces({ onSelectPlace }) {
+  const [availablePlaces, setAvailablePlaces] = useState([]);
+  const [isFetching, setIsFetching] = useState(false);
+  const [error, setError] = useState(false);
+
+  useEffect(() => {
+    setIsFetching(true);
+    async function getPlaces() {
+      try {
+        const places = await fetchPlaces();
+
+        navigator.geolocation.getCurrentPosition((position) => {
+          const sortedPlaces = sortPlacesByDistance(
+            places,
+            position.coords.latitude,
+            position.coords.longitude
+          );
+          setAvailablePlaces(sortedPlaces);
+        });
+
+        setAvailablePlaces(places);
+      } catch (err) {
+        setError(err.message);
+      } finally {
+        setIsFetching(false);
+      }
+    }
+
+    getPlaces();
+  }, []);
+
+  if (error) {
+    return <Error title="An Error " message={error.message} />;
+  }
+
   return (
     <Places
       title="Available Places"
-      places={[]}
+      places={availablePlaces}
+      isLoading={isFetching}
+      loadingtext="loading places..."
       fallbackText="No places available."
       onSelectPlace={onSelectPlace}
     />
---
+++ src/http.js
@@ -0,0 +1,27 @@
+export async function fetchPlaces() {
+  const res = await fetch("http://localhost:3000/places");
+  const data = await res.json();
+
+  if (!res.ok) {
+    throw new Error("Failed to fetch places");
+  }
+
+  return data.places;
+}
+
+export async function updateUserPlaces(places) {
+  const res = await fetch("http://localhost:3000/user-places", {
+    method: "PUT",
+    body: JSON.stringify({ places }),
+    headers: {
+      "Content-Type": "application/json",
+    },
+  });
+
+  const resData = await res.json();
+  if (!res.ok) {
+    throw new Error("Failed to update places");
+  }
+
+  return resData.message;
+}
